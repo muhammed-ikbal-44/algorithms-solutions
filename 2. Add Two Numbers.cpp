134c4ae86f0a52f93fceaa3087cbd90493533fc9
Muhammed <ikbalozbey23.44@gmail.com>
2025-12-30 11:37:56 +0300

    targaH 2

---
+++ 2. Add Two Numbers.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+
+ 
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dumm = new ListNode(0);
+        ListNode* curr=dumm;
+
+        int carry=0;
+
+        while(l1 != nullptr || l2 != nullptr || carry!=0){
+            int sum=carry;
+            if(l1!=nullptr){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2!=nullptr){
+                sum+=l2->val;
+                l2=l2->next;
+            }
+
+            carry=sum/10;
+            /*
+            curr->next= new ListNode(0);
+            curr=curr->next;
+            curr->val=sum%10;*/
+
+            //this piece of code make it almost 0ms, I dont know why
+
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+
+        }
+
+
+        return dumm->next;
+    }
+};
